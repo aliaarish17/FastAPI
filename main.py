3cc2519213ae2b006f52312690db60d31adabd1f
aliaarish17 <aliaarish949@gmail.com>
2026-06-16 22:42:20 +0530

    yo

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import json 
+
+app = FastAPI()
+
+def load_data():
+    with open  ("patients.json", "r") as f:
+        data = json.load(f)
+
+    return data
+
+
+@app.get("/")
+def hello():
+    return{'message': 'Patient management system API'}
+
+@app.get("/about")
+def aboutme():
+    return{"message": "A fully functional API  to manage patients"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
